7aa84599b3a553b0cf99a6069ad5aec665b01a60
cthulhu666 <jakub.gluszecki@gmail.com>
2012-03-03 00:23:21 +0100

    refactoring

--- lib/board.rb
+++ lib/board.rb
@@ -2,35 +2,32 @@ require 'position'
 
 class Board
   
-  def initialize positions
+  def initialize(positions)
     @fields = Array.new
-    add_pieces :white, positions[:white]
-    add_pieces :black, positions[:black]
+    [:white, :black].each { |color| add_pieces(color, positions[color]) }
   end
   
-  def add_pieces color, positions
+  def add_pieces(color, positions)
     positions.each do | k, v | 
-      if v.class==Array
+      if v.is_a?Array
         v.each { |p| add_piece(color, k, p) }
-      elsif v.class==String
-        add_piece(color, k, v)
       else
-        raise "chuj"
+        add_piece(color, k, v)
       end
     end
   end
   
-  def add_piece color, piece, pos
+  def add_piece(color, piece, pos)
     p = Position.create pos
     puts "add_piece #{color} #{piece} #{p.ordinal}"
     @fields[p.ordinal] = [color, piece]
   end
   
-  def is_clear? pos
+  def is_clear?(pos)
     @fields[Position.create(pos).ordinal].nil?
   end
   
-  def [] pos
+  def [](pos)
     return @fields[pos.ordinal] if pos.class==Position
     return @fields[pos] if pos.class==Fixnum
     return @fields[Position.create(pos).ordinal] if pos.class==String
